41e063b8a1a9ed97c95e62ce6cfa047205ed04ab
BK_0710 <34121061+ndsl7109256@users.noreply.github.com>
2020-07-12 00:30:09 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -7,30 +7,107 @@ from keras.utils import to_categorical
 import os
 #import matplotlib.pyplot as plt
 import keras
-from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Lambda
 import random
 from PIL import Image 
 from random import shuffle
-from keras.optimizers import Adam
 
-batch_sz = 16
+batch_sz = 4
 oti = 'adam'
 lr = 0.0001
-e_num = 50
+e_num = 10
+
+
+# class Histories(Callback):
+
+#     def on_train_begin(self,logs={}):
+#         self.losses = []
+#         self.accuracies = []
+
+#     def on_batch_end(self, batch, logs={}):
+#         self.losses.append(logs.get('loss'))
+#         self.accuracies.append(logs.get('acc'))
+
+#https://github.com/keras-team/keras-contrib/blob/master/keras_contrib/layers/convolutional/subpixelupscaling.py
+#https://github.com/twairball/keras-subpixel-conv
+def SubpixelConv2D(input_shape, scale=4):
+    """
+    Keras layer to do subpixel convolution.
+    NOTE: Tensorflow backend only. Uses tf.depth_to_space
+    Ref:
+        [1] Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network
+            Shi et Al.
+            https://arxiv.org/abs/1609.05158
+    :param input_shape: tensor shape, (batch, height, width, channel)
+    :param scale: upsampling scale. Default=4
+    :return:
+    """
+    # upsample using depth_to_space
+    def subpixel_shape(input_shape):
+        dims = [input_shape[0],
+                input_shape[1] * scale,
+                input_shape[2] * scale,
+                int(input_shape[3] / (scale ** 2))]
+        output_shape = tuple(dims)
+        return output_shape
+
+    def subpixel(x):
+        import tensorflow as tf 
+        return tf.nn.depth_to_space(x, scale)
+
+
+    return Lambda(subpixel, output_shape=subpixel_shape, name='subpixel')
+
+def SubpixelConv2D2(input_shape, scale=4):
+    """
+    Keras layer to do subpixel convolution.
+    NOTE: Tensorflow backend only. Uses tf.depth_to_space
+    Ref:
+        [1] Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network
+            Shi et Al.
+            https://arxiv.org/abs/1609.05158
+    :param input_shape: tensor shape, (batch, height, width, channel)
+    :param scale: upsampling scale. Default=4
+    :return:
+    """
+    # upsample using depth_to_space
+    def subpixel_shape(input_shape):
+        dims = [input_shape[0],
+                input_shape[1] * scale,
+                input_shape[2] * scale,
+                int(input_shape[3] / (scale ** 2))]
+        output_shape = tuple(dims)
+        return output_shape
+
+    def subpixel2(x):
+        import tensorflow as tf 
+        return tf.nn.depth_to_space(x, scale)
+
+
+    return Lambda(subpixel2, output_shape=subpixel_shape, name='subpixel_2')
+
+
+#http://ethen8181.github.io/machine-learning/keras/resnet_cam/resnet_cam.html
+
+
+##https://ithelp.ithome.com.tw/articles/10223034
 
 
 def main():
-  # With .npy read directly 
   # train_image = np.load('train_image.npy')
   # train_label = np.load('train_label.npy')
+
+  # test_image = np.load('test_img.npy')
+  # test_label = np.load('test_lab.npy')
+
 
   train_image = []
   train_label = []
 
-  entries = os.listdir('./patch')
+  entries = os.listdir('./p')
   for entry in entries:
-    im = image.load_img('./patch/'+entry, target_size = (64, 64))
+    im = image.load_img('./p/'+entry, target_size = (64, 64))
     img = image.img_to_array(im)
     img = img[:,:,0]
     img = img[:,:,np.newaxis]
@@ -40,9 +117,9 @@ def main():
   print(train_image.shape)# (x,128,128,1)
   # np.save('train_image',train_image)
 
-  entries = os.listdir('./label')
+  entries = os.listdir('./l')
   for entry in entries:
-    im = image.load_img('./label/'+entry, target_size = (128, 128))
+    im = image.load_img('./l/'+entry, target_size = (128, 128))
     img = image.img_to_array(im)
     train_label.append(img)
   train_label = np.stack(train_label)
@@ -50,9 +127,7 @@ def main():
   print(train_label.shape)# (x,256,256,3)
   
   # np.save('train_label',train_label)
-  
 
-  #Shuffle
   index = [i for i in range(train_image.shape[0])]
   shuffle(index)
   train_image = train_image[index,:,:,:];
@@ -73,6 +148,22 @@ def main():
   #x = SubpixelConv2D(input_shape=(64,64,256), scale=2)(x)
   sub_layer = Lambda(lambda x:tf.nn.depth_to_space(x,2))
   x = sub_layer(inputs=x)
+  
+  # x = keras.layers.Conv2DTranspose(filters = 64, kernel_size = 4,
+  # 						strides=2,
+  # 						padding='same',
+  # 						output_padding=None, 
+  # 						data_format=None, 
+  # 						dilation_rate=(1, 1), 
+  # 						activation='relu', 
+  # 						use_bias=True, 
+  # 						kernel_initializer='glorot_uniform', 
+  # 						bias_initializer='zeros', 
+  # 						kernel_regularizer=None, 
+  # 						bias_regularizer=None, 
+  # 						activity_regularizer=None, 
+  # 						kernel_constraint=None, 
+  # 						bias_constraint=None)(x)
   
   ####Conv, PReLU 
   x = keras.layers.Conv2D(filters = 256, #feature map number
@@ -103,6 +194,8 @@ def main():
    
     
     x = keras.layers.Add()([stage_2_Conv1_2,x])
+    x = keras.layers.PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=[1,2])(x)
+
 
     
   ##STAGE 3
@@ -110,6 +203,22 @@ def main():
   #x = SubpixelConv2D2((64,64,256), scale=2)(x)
   sub_layer = Lambda(lambda x:tf.nn.depth_to_space(x,2))
   x = sub_layer(inputs=x)
+  
+  # x = keras.layers.Conv2DTranspose(filters = 64, kernel_size = 3,
+  # 						strides=2, 
+  # 						padding='same',
+  # 						output_padding=(1,1),
+  # 						data_format=None, 
+  # 						dilation_rate=(1, 1), 
+  # 						activation='relu', 
+  # 						use_bias=True, 
+  # 						kernel_initializer='glorot_uniform', 
+  # 						bias_initializer='zeros', 
+  # 						kernel_regularizer=None, 
+  # 						bias_regularizer=None, 
+  # 						activity_regularizer=None, 
+  # 						kernel_constraint=None, 
+  # 						bias_constraint=None)(stage_2_Concat_2)
   
   ####Conv, PReLU
   x = keras.layers.Conv2D(filters = 256, #feature map number
@@ -127,18 +236,19 @@ def main():
                      activation ='relu',
                      input_shape = (None,None,64))(x)
 
-  rrp = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=1, mode='min', min_lr=0.0000001)
 
  
   model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
   model.summary()
-  model.compile(optimizer=Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08), loss = 'mean_squared_error', metrics = ['mse'])
+  model.compile(optimizer=keras.optimizers.Nadam(lr), loss = 'mean_squared_error', metrics = ['mse'])
+  #histories = Histories()
   checkpoint = ModelCheckpoint('model.hdf5',verbose=1, monitor='val_loss', 
                                 save_best_only=True,save_weights_only=True)
   history = model.fit(train_image, train_label, epochs=e_num, batch_size=batch_sz,verbose=1,
-              validation_split = 0.1,callbacks=[checkpoint,rrp],shuffle = True)
-  model.save("Demosaic.h5")
-
+              validation_split = 0.1,callbacks=[checkpoint],shuffle = True)
+  model.save("trashn.h5")
+  # loss, accuracy = model.evaluate(test_image,test_label)
+  # print(loss)
   
 
 
